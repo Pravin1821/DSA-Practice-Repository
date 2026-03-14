f9ba126de1cced79e9e92d4e161dd3c6b7950a40
Pravin1821 <pravincriss21@gmail.com>
2026-03-14 23:39:18 +0530

    Array - Q

---
+++ Arrays/ContainsMostWater.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ContainsMostWater {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while(left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+            maxArea = Math.max(maxArea, area);
+            if(height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] height = new int[n];
+        for(int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+        int result = maxArea(height);
+        System.out.println(result);
+    }
+}
